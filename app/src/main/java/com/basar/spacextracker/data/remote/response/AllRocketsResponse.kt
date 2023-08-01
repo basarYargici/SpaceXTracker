0ccaa62af8c0eb783f0aef4b6070aa77b1cbaba0
basarYargici <ibrahimbyargici@gmail.com>
2023-08-01 04:32:05 +0300

    add internet permission

--- app/src/main/java/com/basar/spacextracker/data/remote/response/AllRocketsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.basar.spacextracker.data.remote.response
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class AllRocketsResponse(
-    val rockets: List<Rocket>?
-) : Parcelable
